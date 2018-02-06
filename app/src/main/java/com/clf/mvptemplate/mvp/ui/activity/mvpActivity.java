38b0b66ae9b5c2991e6d3b6e07b97dfd05a8214b
hacker <crazycode>
2018-02-06 15:01:37 +0800

    init project

---
+++ app/src/main/java/com/clf/mvptemplate/mvp/ui/activity/mvpActivity.java
@@ -0,0 +1,15 @@
+package com.clf.mvptemplate.mvp.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.clf.mvptemplate.R;
+
+public class mvpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mvp);
+    }
+}
